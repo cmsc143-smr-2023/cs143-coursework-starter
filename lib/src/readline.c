6ca435dbd94f67b96cdfb8de519cbf5e64b6ed5c
Byron Zhong <zhongbangyuan@gmail.com>
2023-07-19 13:24:25 -0500

    add readline solution

--- lib/src/readline.c
+++ lib/src/readline.c
@@ -1,21 +1,37 @@
 /* implementation of readline.h module
  *
- * Author: YOUR NAME HERE
+ * Author: Byron Zhong (byronzhong@uchicago.edu)
  */
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "readline.h"
+#include "dynamic-buffer.h"
 
 int readline(FILE *file, char **line_p)
 {
         assert(file != NULL && line_p != NULL);
 
-        /* ... YOUR CODE HERE ... */
+        struct buffer buf;
+        buffer_init(&buf, 32);
 
-        assert(0 && "TODO");
+        int c;
+        while ((c = fgetc(file)) != EOF) {
+                buffer_append(&buf, c);
+                if (c == '\n') {
+                        break;
+                }
+        }
 
-        return 42;
+        int len = buffer_length(&buf);
+        if (len == 0) {
+                free(buf.start);
+                *line_p = NULL;
+                return 0;
+        }
+
+        buffer_append(&buf, '\0');
+        *line_p = buf.start;
+        return len;
 }
-
