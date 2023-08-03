bed4a75450ee72db07ce1b0d6d8a58425b2facce
Byron Zhong <zhongbangyuan@gmail.com>
2023-08-03 12:31:47 -0500

    add solution

--- hw6/compress.c
+++ hw6/compress.c
@@ -6,17 +6,9 @@
 #include <string.h>
 #include <stdbool.h>
 
-/* prints the usage message and exit the program */
 static void usage(const char *prog_name);
 
-/* Given an input file, compress the records in the file and output the result
- * to the standard output
- */
 void compress(FILE *file);
-
-/* Given an input file, extract the records and output the result to the
- * standard output
- */
 void extract(FILE *file);
 
 int main(int argc, char *argv[])
@@ -25,7 +17,6 @@ int main(int argc, char *argv[])
                 usage(argv[0]);
         }
 
-        /* parse the first command-line argument */
         bool to_compress = false;
         if (strcmp(argv[1], "-c") == 0 || strcmp(argv[1], "--compress") == 0) {
                 to_compress = true;
@@ -36,7 +27,6 @@ int main(int argc, char *argv[])
                 usage(argv[0]);
         }
 
-        /* parse the second command-line argument */
         FILE *file;
         if (strcmp(argv[2], "-") == 0) {
                 file = stdin;
@@ -72,15 +62,44 @@ static void usage(const char *prog_name)
         exit(EXIT_FAILURE);
 }
 
+const uint64_t ONES = ~(uint64_t) 0;
+const uint64_t UCID_MASK = ONES >> (64 - 24) << 15;
+const uint64_t LATE_MASK = ONES >> (64 - 2) << 13;
+const uint64_t POINT_MASK = ONES >> (64 - 4) << 9;
+const uint64_t GRADE_MASK = ONES >> (64 - 3) << 6;
+const uint64_t MAJOR_MASK = ONES >> (64 - 6);
+
 const char FILE_HEADER[] = "CS143 student binary format\n";
 
 void compress(FILE *file)
 {
-        /* output the file header for compressed format */
         printf("%s", FILE_HEADER);
 
-        (void) file;
-        assert(0 && "todo");
+        struct student *s;
+        while ((s = student_read(file)) != NULL) {
+                uint64_t word = 0;
+
+                uint64_t ucid = s->ucid - 10000000;
+                word |= ucid << 15;
+                word |= (uint64_t) s->late_days << 13;
+                word |= ((int64_t) s->point_adjust << 9) & POINT_MASK;
+
+                uint64_t grade = (s->grade == 'F') ? 4 : s->grade - 'A';
+                word |= grade << 6;
+
+                uint64_t major = s->major;
+                word |= major;
+
+                for (int i = 4; i >= 0; i--) {
+                        uint8_t byte = word >> (i * 8);
+                        putchar(byte);
+                }
+
+                for (int i = 0; i < 17; i++) {
+                        putchar(s->hometown[i]);
+                }
+                student_free(s);
+        }
 }
 
 void extract(FILE *file)
@@ -96,6 +115,37 @@ void extract(FILE *file)
         }
         free(header_line);
 
-        assert(0 && "todo");
+        while (!feof(file)) {
+                uint64_t word = 0;
+                for (int i = 4; i >= 0; i--) {
+                        int byte = fgetc(file);
+                        if (byte == EOF) {
+                                return;
+                        }
+                        word |= (uint64_t) byte << (i * 8);
+                }
+
+                char hometown[18] = { 0 };
+                for (int i = 0; i < 17; i++) {
+                        int byte = fgetc(file);
+                        assert(byte != EOF);
+                        hometown[i] = byte;
+                }
+
+                uint32_t ucid = ((word & UCID_MASK) >> 15) + 10000000;
+                uint8_t late_days = (word & LATE_MASK) >> 13;
+                int8_t point_adjust = (int64_t) (word & POINT_MASK) << 51 >> 60;
+                char grade = ((word & GRADE_MASK) >> 6) + 'A';
+                if (grade == 'E') {
+                        grade = 'F';
+                }
+
+                enum major major = word & MAJOR_MASK;
+
+                struct student *s = student_create(ucid, late_days,
+                                point_adjust, grade, major, hometown);
+                student_write(s, stdout);
+                student_free(s);
+        }
 }
 
